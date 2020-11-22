256847185d16c810cdaa60adcfcc867d0eaacab8
Yajush Vyas <35025375+geekyvyas@users.noreply.github.com>
2020-11-23 00:33:30 +0530

    Update PlayP.py
    done changes...but maine run nahi kara hai..tu run karaoke dekh Lio

--- PlayP.py
+++ PlayP.py
@@ -1,16 +1,9 @@
-from cv2 import cv2
+from keras.models import load_model
+import cv2
 import numpy as np
-from keras_squeezenet import SqueezeNet
-from keras.optimizers import Adam
-from keras.utils import np_utils
-from keras.layers import Activation, Dropout, Convolution2D, GlobalAveragePooling2D
-from keras.models import Sequential
-import tensorflow as tf
-import os
+from random import choice
 
-IMG_SAVE_PATH = 'image_data'
-
-CLASS_MAP = {
+REV_CLASS_MAP = {
     "one": 1,
     "two": 2,
     "three": 3,
@@ -20,71 +13,177 @@ CLASS_MAP = {
     "none":7
 }
 
-NUM_CLASSES = len(CLASS_MAP)
-
 
 def mapper(val):
     return CLASS_MAP[val]
 
 
-def get_model():
-    model = Sequential([
-        SqueezeNet(input_shape=(227, 227, 3), include_top=False),
-        Dropout(0.5),
-        Convolution2D(NUM_CLASSES, (1, 1), padding='valid'),
-        Activation('relu'),
-        GlobalAveragePooling2D(),
-        Activation('softmax')
-    ])
-    return model
+def calculate_winner(move1, move2, sum):
+    if move1 == move2:
+        out = 810498
+        return out
 
+    if move1 == "one":
+        if move2 == "two":
+            sum = sum + 3
+            return sum
+        if move2 == "three":
+            sum = sum + 4
+            return sum
+        if move2 == "four":
+            sum = sum + 5
+            return sum
+        if move2 == "five":
+            sum = sum + 6
+            return sum
+        if move2 == "six":
+            sum = sum + 7
+            return sum
 
-# load images from the directory
-dataset = []
-for directory in os.listdir(IMG_SAVE_PATH):
-    path = os.path.join(IMG_SAVE_PATH, directory)
-    if not os.path.isdir(path):
+    if move1 == "two":
+        if move2 == "one":
+            sum = sum + 3
+            return sum
+        if move2 == "three":
+            sum = sum + 5
+            return sum
+        if move2 == "four":
+            sum = sum + 6
+            return sum
+        if move2 == "five":
+            sum = sum + 7
+            return sum
+        if move2 == "six":
+            sum = sum + 8
+            return sum
+
+    if move1 == "three":
+        if move2 == "one":
+            sum = sum + 4
+            return sum
+        if move2 == "two":
+            sum = sum + 5
+            return sum
+        if move2 == "four":
+            sum = sum + 7
+            return sum
+        if move2 == "five":
+            sum = sum + 8
+            return sum
+        if move2 == "six":
+            sum = sum + 9
+            return sum
+
+     if move1 == "four":
+        if move2 == "one":
+            sum = sum + 5
+            return sum
+        if move2 == "two":
+            sum = sum + 6
+            return sum
+        if move2 == "three":
+            sum = sum + 7
+            return sum
+        if move2 == "five":
+            sum = sum + 9
+            return sum
+        if move2 == "six":
+            sum = sum + 10
+            return sum
+        
+     if move1 == "five":
+        if move2 == "one":
+            sum = sum + 6
+            return sum
+        if move2 == "two":
+            sum = sum + 7
+            return sum
+        if move2 == "three":
+            sum = sum + 8
+            return sum
+        if move2 == "four":
+            sum = sum + 9
+            return sum
+        if move2 == "six":
+            sum = sum + 11
+            return sum
+        
+     if move1 == "six":
+        if move2 == "one":
+            sum = sum + 7
+            return sum
+        if move2 == "two":
+            sum = sum + 8
+            return sum
+        if move2 == "three":
+            sum = sum + 9
+            return sum
+        if move2 == "four":
+            sum = sum + 10
+            return sum
+        if move2 == "five":
+            sum = sum + 11
+            return sum
+        
+        
+model = load_model("Hand-cricket2-model.h5")
+
+cap = cv2.VideoCapture(0)
+
+prev_move = None
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
         continue
-    for item in os.listdir(path):
-        # to make sure no hidden files get in our way
-        if item.startswith("."):
-            continue
-        img = cv2.imread(os.path.join(path, item))
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        img = cv2.resize(img, (227, 227))
-        dataset.append([img, directory])
+        
+    # rectangle for user to play
+    cv2.rectangle(frame, (100, 100), (500, 500), (255, 255, 255), 2)
+    # rectangle for computer to play
+    cv2.rectangle(frame, (800, 100), (1200, 500), (255, 255, 255), 2)
 
-'''
-dataset = [
-    [[...], 'one'],
-    [[...], 'two'],
-    ...
-]
-'''
-data, labels = zip(*dataset)
-labels = list(map(mapper, labels))
+    # extract the region of image within the user rectangle
+    roi = frame[100:500, 100:500]
+    img = cv2.cvtColor(roi, cv2.COLOR_BGR2RGB)
+    img = cv2.resize(img, (227, 227))
 
+    # predict the move made
+    pred = model.predict(np.array([img]))
+    move_code = np.argmax(pred[0])
+    user_move_name = mapper(move_code)
 
-'''
-labels: one,two,three...
-one hot encoded: [1,0,0,0,0,0,0], [0,1,0,0,0,0,0], [0,0,1,0,0,0,0],...
-'''
+    # predict the winner (human vs computer)
+    if prev_move != user_move_name:
+        sum = 0
+        if user_move_name != "none":
+            computer_move_name = choice(['one', 'two', 'three','four','fivr','six'])
+            winer = sum
+            winner = calculate_winner(user_move_name, computer_move_name,sum)
+        else:
+            computer_move_name = "none"
+            winner = 0
+    prev_move = user_move_name
 
-# one hot encode the labels
-labels = np_utils.to_categorical(labels)
+    # display the information
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    cv2.putText(frame, "Your Move: " + user_move_name,
+                (50, 50), font, 1.2, (255, 255, 255), 2, cv2.LINE_AA)
+    cv2.putText(frame, "Computer's Move: " + computer_move_name,
+                (750, 50), font, 1.2, (255, 255, 255), 2, cv2.LINE_AA)
+    cv2.putText(frame, "Winner: " + winner,
+                (400, 600), font, 2, (0, 0, 255), 4, cv2.LINE_AA)
 
-# define the model
-model = get_model()
-model.compile(
-    optimizer=Adam(lr=0.0001),
-    loss='categorical_crossentropy',
-    metrics=['accuracy']
-)
+    if computer_move_name != "none":
+        icon = cv2.imread(
+            "images/{}.png".format(computer_move_name))
+        icon = cv2.resize(icon, (400, 400))
+        frame[100:500, 800:1200] = icon
 
-# start training
-model.fit(np.array(data), np.array(labels), epochs=10)
+    cv2.imshow("Rock Paper Scissors", frame)
 
-# save the model for later use
-model.save("Hand-cricket2-model.h5")
+    k = cv2.waitKey(10)
+    if k == ord('q'):
+        break
 
-
+cap.release()
+cv2.destroyAllWindows()
