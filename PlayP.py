9289b2047cb2b0cff92d4065883b16bedd993b87
Aditya Narayan Sharma <37697073+Adinarayanreloaded@users.noreply.github.com>
2020-11-22 21:54:17 +0530

    Update PlayP.py

--- PlayP.py
+++ PlayP.py
@@ -1,81 +1,89 @@
-from keras.models import load_model
 from cv2 import cv2
 import numpy as np
-from random import choice
+from keras_squeezenet import SqueezeNet
+from keras.optimizers import Adam
+from keras.utils import np_utils
+from keras.layers import Activation, Dropout, Convolution2D, GlobalAveragePooling2D
+from keras.models import Sequential
+import tensorflow as tf
+import os
+IMG_SAVE_PATH = 'image_data'
 
-REV_CLASS_MAP = {
-    1: "one",
-    2: "two",
-    3: "three",
-    4: "four",
-    5: "five",
-    6: "six",
-    7: "none"
+CLASS_MAP = {
+    "one": 1,
+    "two": 2,
+    "three": 3,
+    "four": 4,
+    "five": 5,
+    "six": 6,
+    "none":7
 }
+
+NUM_CLASSES = len(CLASS_MAP)
 
 
 def mapper(val):
-    return REV_CLASS_MAP[val]
+    return CLASS_MAP[val]
 
-model = load_model("Hand-cricket2-model.h5")
 
-cap = cv2.VideoCapture(0)
-cap.set(3,1280)# 3-PROPERTY index for WIDTH
-cap.set(4,720)# 4-PROPERTY index for HEIGHT
+def get_model():
+    model=tf.keras.Sequential([
+    tf.keras.layers.Conv2D(16,(3,3),input_shape=(227,227,3),activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    tf.keras.layers.Conv2D(16,(3,3),activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(20,activation='relu'),
+    tf.keras.layers.Dense(7,activation='softmax')
+    ])
+    return model 
 
-prev_move = None
 
-while True:
-    ret, frame = cap.read()
-    if not ret:
+# load images from the directory
+dataset = []
+for directory in os.listdir(IMG_SAVE_PATH):
+    path = os.path.join(IMG_SAVE_PATH, directory)
+    if not os.path.isdir(path):
         continue
+    for item in os.listdir(path):
+        # to make sure no hidden files get in our way
+        if item.startswith("."):
+            continue
+        img = cv2.imread(os.path.join(path, item))
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        img = cv2.resize(img, (227, 227))
+        dataset.append([img, directory])
 
-    # rectangle for user to play
-    cv2.rectangle(frame, (100, 100), (500, 500), (255, 255, 255), 2)
-    # rectangle for computer to play
-    cv2.rectangle(frame, (800, 100), (1200, 500), (255, 255, 255), 2)
+'''
+dataset = [
+    [[...], 'one'],
+    [[...], 'two'],
+    ...
+]
+'''
+data, labels = zip(*dataset)
+labels = list(map(mapper, labels))
 
-    # extract the region of image within the user rectangle
-    roi = frame[100:500, 100:500]
-    img = cv2.cvtColor(roi, cv2.COLOR_BGR2RGB)
-    img = cv2.resize(img, (227, 227))
 
-    # predict the move made
-    pred = model.predict(np.array([img]))
-    move_code = np.argmax(pred[0])
-    user_move_name = mapper(move_code)
+'''
+labels: one,two,three...
+one hot encoded: [1,0,0,0,0,0,0], [0,1,0,0,0,0,0], [0,0,1,0,0,0,0],...
+'''
 
-    # predict the winner (human vs computer)
-    if prev_move != user_move_name:
-        if user_move_name != "none":
-            computer_move_name = choice(['one', 'two', 'three', 'four', 'five', 'six'])
-            sum=sum+user_move_name
-        else:
-            computer_move_name = "none"
-            cv2.putText(frame,"Waiting")
-                       (400,600), font, 2, (0,0,225), 4, cv2.Line_AA)
-    prev_move = user_move_name
+# one hot encode the labels
+labels = np_utils.to_categorical(labels)
 
-    # display the information
-    font = cv2.FONT_HERSHEY_SIMPLEX
-    cv2.putText(frame, "Your Move: " + user_move_name,
-                (50, 50), font, 1.2, (255, 255, 255), 2, cv2.LINE_AA)
-    cv2.putText(frame, "Computer's Move: " + computer_move_name,
-                (750, 50), font, 1.2, (255, 255, 255), 2, cv2.LINE_AA)
-    cv2.putText(frame, "Sum:" + sum,
-                (400, 600), font, 2, (0, 0, 255), 4, cv2.LINE_AA)
+# define the model
+model = get_model()
+model.compile(
+    optimizer=Adam(lr=0.0001),
+    loss='categorical_crossentropy',
+    metrics=['accuracy']
+)
 
-    if computer_move_name != "none":
-        icon = cv2.imread(
-            "images/{}.png".format(computer_move_name))
-        icon = cv2.resize(icon, (400, 400))
-        frame[100:500, 800:1200] = icon
+# start training
+model.fit(np.array(data), np.array(labels), epochs=10)
 
-    cv2.imshow("Hand Cricket Frame", frame)
+# save the model for later use
+model.save("Hand-cricket2-model.h5")
 
-    k = cv2.waitKey(10)
-    if k == ord('q'):
-        break
-
-cap.release()
-cv2.destroyAllWindows()
